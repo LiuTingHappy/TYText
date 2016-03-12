c69c1f8a8b76855c085c8218e54e435085d9d445
LiuTingHappy <mac@MacdeiMac-77.local>
2016-03-12 15:29:43 +0800

    Liu
    liu

---
+++ Liu123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Liu123
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 Liuting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
